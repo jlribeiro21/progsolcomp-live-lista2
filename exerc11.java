bd8c7cd22bf338f84cf50091874b808ad3c24641
jlribeiro21 <128436000+jlribeiro21@users.noreply.github.com>
2023-05-21 22:45:24 -0300

    Update exerc11.java

--- exerc11.java
+++ exerc11.java
@@ -1,3 +1,9 @@
+/* Nome do Aluno: José Lucas Augusto Ribeiro
+* RA: 323114228
+* Nome do Programa: Exercicio 11
+* Data: 21.05.2023
+*/
+
 public class exerc11 {
     public static void main(String[] args) {
         for (int i = 100; i >= 1; i--) {

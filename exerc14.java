03eac4d15fd6a91b4e55557ace8551e3b2d7e60d
jlribeiro21 <128436000+jlribeiro21@users.noreply.github.com>
2023-05-21 22:46:23 -0300

    Update exerc14.java

--- exerc14.java
+++ exerc14.java
@@ -1,3 +1,9 @@
+/* Nome do Aluno: José Lucas Augusto Ribeiro
+* RA: 323114228
+* Nome do Programa: Exercicio 14
+* Data: 21.05.2023
+*/
+
 public class exerc14 {
     public static void main(String[] args) {
         int tamanho = 50;

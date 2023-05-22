d78df94509ab87a68d35242acd46ec503bca63f6
jlribeiro21 <128436000+jlribeiro21@users.noreply.github.com>
2023-05-21 22:47:08 -0300

    Update exerc17.java

--- exerc17.java
+++ exerc17.java
@@ -1,3 +1,9 @@
+/* Nome do Aluno: José Lucas Augusto Ribeiro
+* RA: 323114228
+* Nome do Programa: Exercicio 17
+* Data: 21.05.2023
+*/
+
 public class exerc17 {
     public static void main(String[] args) {
         int numero = -7;
